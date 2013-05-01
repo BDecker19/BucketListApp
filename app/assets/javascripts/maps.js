957df17ccd3501bad424d9c4a9b254bd27cf70d9
Dan Garland <dan@dangarland.co.uk>
2013-05-01 14:22:13 -0400

    Adding event listeners

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -7,6 +7,19 @@ function initMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+
+  google.maps.event.addListener(map, "projection_changed", function() {
+    console.log("hi");
+  });
+
+  google.maps.event.addListener(map, "zoom_changed", function() {
+    console.log(map.getZoom());
+  });
+
+  google.maps.event.addListener(map, "center_changed", function() {
+    console.log(map.getBounds().getSouthWest());
+    console.log(map.getBounds().getNorthEast());
+  });
 }
 
 function addMarker(latitude, longitude, title) {
