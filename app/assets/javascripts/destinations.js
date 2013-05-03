1eeb6cb7c774d0250f8f82f613d3029599ae4d07
Dan Garland <dan@dangarland.co.uk>
2013-05-03 09:57:05 -0400

    Search box

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('.toggle-comment').click(function() {
+  $('body').on('click', '.toggle-comment', function() {
+//  $('.toggle-comment').click(function() {
     $(this).next('.new-comment').slideToggle();
   });
 });
